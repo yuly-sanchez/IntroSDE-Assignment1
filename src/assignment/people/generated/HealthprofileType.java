6ead7ac2b533e0766af3afbf699a4a7d270063fa
yuly-sanchez <yuly.sanchez29@gmail.com>
2015-10-21 10:53:31 +0200

    first src folder

--- src/assignment/people/generated/HealthprofileType.java
+++ src/assignment/people/generated/HealthprofileType.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.7 
 // Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2015.10.20 alle 10:47:17 PM CEST 
+// Generato il: 2015.10.21 alle 10:49:01 AM CEST 
 //
 
 
